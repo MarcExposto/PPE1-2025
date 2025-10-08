fc583f6d7553e0a452e411babfdff099920443f2
MarcExposto <marcexposto@outlook.com>
2025-10-08 10:58:03 +0200

    Ajout de comptes.sh de l'exercice 1 du TP1

---
+++ Exercices/comptes.sh
@@ -0,0 +1,7 @@
+echo "nombre de lieux en 2016: "  #> location_2016.txt |
+cat 2016/* |grep Location | wc -l # >> location_2016.txt
+#cat loc_2016.txt
+echo "nombre de lieux en 2017: "  #> location_2017.txt |
+cat 2017/* |grep Location | wc -l #>> location_2017.txt
+echo "nombre de lieux en 2018: " # > location_2018.txt |
+cat 2018/* |grep Location | wc -l #>> location_2018.txt
