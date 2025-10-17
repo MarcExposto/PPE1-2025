e19536d54607815f095ff51604953588634befa4
MarcExposto <marcexposto@outlook.com>
2025-10-17 16:40:00 +0200

    Version fini des exercices scripts

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,7 +1,16 @@
-echo "nombre de lieux en 2016: "  #> location_2016.txt |
-cat 2016/* |grep Location | wc -l # >> location_2016.txt
-#cat loc_2016.txt
-echo "nombre de lieux en 2017: "  #> location_2017.txt |
-cat 2017/* |grep Location | wc -l #>> location_2017.txt
-echo "nombre de lieux en 2018: " # > location_2018.txt |
-cat 2018/* |grep Location | wc -l #>> location_2018.txt
+#!/usr/bin/bash
+echo "Argument donné (le chemin) : $1"
+
+CHEMIN=$1  # Ne pas mettre d'espaces lors de l'affectation !
+
+if [ -n "$CHEMIN" ];
+    then
+    for a in 2016 2017 2018;
+        do
+        echo "Nombre de lieux en $a :"
+        cat "$CHEMIN"/$a/* | grep Location | wc -l
+    done
+else
+    echo "Veuillez relancer et spécifier un chemin"
+    exit
+fi
